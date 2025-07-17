4754de30a923ea7717ca4672639849349e1d5f4b
honi20 <1594cyh@gmail.com>
2025-07-17 14:12:33 +0900

    initial commit

---
+++ sts/test.sh
@@ -0,0 +1,11 @@
+URL=http://localhost:8080
+
+curl -X POST -d "hello1" $URL
+curl -X POST -d "hello2" $URL
+curl -X POST -d "hello3" $URL
+curl -X POST -d "hello4" $URL
+curl -X POST -d "hello5" $URL
+curl -X POST -d "hello6" $URL
+curl -X POST -d "hello7" $URL
+curl -X POST -d "hello8" $URL
+curl -X POST -d "hello9" $URL
